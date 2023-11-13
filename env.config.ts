38afc75256c7f745d5f8df6995b3913503e178d3
Dimas Knitto <it08.knitto@gmail.com>
2023-11-13 13:13:05 +0700

    add case connection and add api testing

--- env.config.ts
+++ env.config.ts
@@ -2,4 +2,7 @@ import 'dotenv/config'
 
 export default {
     rabbitMqUrl: process.env.RABBIT_MQ_URL ?? '',
+    rabbitmqHttp: process.env.RABBITMQ_HTTP ?? '',
+    rabbitmqUser: process.env.RABBITMQ_USER ?? '',
+    rabbitmqPass: process.env.RABBITMQ_PASS ?? ''
 }
